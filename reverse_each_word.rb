79c7d98542d394bafafb327b0f8790926e060cc6
Ashleigh Chun <ashleigh.chun@wework.com>
2018-10-09 04:11:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,14 @@ def reverse_each_word(sentence)
   end
   backwards
 end
+
+
+turn the string into an array. each word is an element in an array.
+for each element, reverse it then shovel it into a new array.
+call that array.
+
+
+
+turn the string into an array,
+collect the elements and reverse.
+

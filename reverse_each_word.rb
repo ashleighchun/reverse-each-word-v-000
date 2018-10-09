b127bb99152ee7465717cdee449af0d9d247f337
Ashleigh Chun <ashleigh.chun@wework.com>
2018-10-09 04:29:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,7 +15,7 @@ def reverse_each_word(sentence)
   sentence_array.collect do |word|
     word.reverse
   end
-  sentence_array.join
+
 end
 
 #turn the string into an array. each word is an element in an array.

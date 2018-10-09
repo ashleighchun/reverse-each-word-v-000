865135545df3764ce89ee0063db858c38dadcaad
Ashleigh Chun <ashleigh.chun@wework.com>
2018-10-09 04:21:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,6 +10,13 @@ def reverse_each_word(sentence)
   backwards.join(" ")
 end
 
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(/ /)
+  sentence_array.collect do |word|
+    backwards << word.reverse
+  end
+  backwards.join(" ")
+end
 
 #turn the string into an array. each word is an element in an array.
 #for each element, reverse it then shovel it into a new array.

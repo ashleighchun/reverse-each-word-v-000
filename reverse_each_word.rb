6297d7d501202a14da4e72eb66a88fc2857cf915
Ashleigh Chun <ashleigh.chun@wework.com>
2018-10-09 04:22:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
 require "pry"
 
 
-def reverse_each_word(sentence)
-  backwards = []
-  sentence_array = sentence.split(/ /)
-  sentence_array.each do |word|
-    backwards << word.reverse
-  end
-  backwards.join(" ")
-end
+#def reverse_each_word(sentence)
+ # backwards = []
+  #sentence_array = sentence.split(/ /)
+  #sentence_array.each do |word|
+    #backwards << word.reverse
+ # end
+  #backwards.join(" ")
+#end
 
 def reverse_each_word(sentence)
   sentence_array = sentence.split(/ /)
